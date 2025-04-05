daedf025dc94740344e1c4635c684098ca16c062
TUGHLOK A. SIDDIQUEE <134185146+TughlokS@users.noreply.github.com>
2025-04-04 22:28:17 -0400

    ,

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/', // Using root path for local development
+  base: '/',
   plugins: [react()],
 })
